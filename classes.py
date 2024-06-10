280ffa4591df8bbd2be8494c6324d2edb55620d4
Devana <devanaemmanuel@gmail.com>
2024-06-10 15:21:20 +0530

    commit

--- classes.py
+++ classes.py
@@ -1,9 +1,16 @@
-class mca_sem3 :
-    def __init__(self,classrep1,classrep2):
-        self.name1 = classrep1
-        self.name2 = classrep2
-    def func1(self):
-        print("ClassRep 1: " + self.name1 + "\nClassrep 2: " + self.name2)
+class mca_sem : 
+    name = "revathi"
 
-mca = mca_sem3("SIVANAND","REVATHI")
-mca.func1()
+rep1 = mca_sem()
+print(rep1.name)
+
+
+# class mca_sem3 :
+#     def __init__(self,classrep1,classrep2):
+#         self.name1 = classrep1
+#         self.name2 = classrep2
+#     def func1(self):
+#         print("ClassRep 1: " + self.name1 + "\nClassrep 2: " + self.name2)
+
+# mca = mca_sem3("SIVANAND","REVATHI")
+# mca.func1()
