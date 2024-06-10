58297bcababe5889f9bd8b01af154e2e2eb905c9
Devana <devanaemmanuel@gmail.com>
2024-06-10 15:14:20 +0530

    commit

---
+++ classes.py
@@ -0,0 +1,9 @@
+class mca_sem3 :
+    def __init__(self,classrep1,classrep2):
+        self.name1 = classrep1
+        self.name2 = classrep2
+    def func1(self):
+        print("ClassRep 1: " + self.name1 + "\nClassrep 2: " + self.name2)
+
+mca = mca_sem3("SIVANAND","REVATHI")
+mca.func1()
