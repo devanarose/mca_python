1515b7a9da80cb9f4725d8bb55d9160f8f5581af
Devana <devanaemmanuel@gmail.com>
2024-06-10 14:28:56 +0530

    commit

--- file2.py
+++ file2.py
@@ -1,2 +1,6 @@
 listt = ['devana','adithya','aswin','sivanand','dhaivath']
 if 'adithya' in listt : print("present in the list") 
+else : print("not present")
+
+listt.sort()
+for i in listt : print(listt)
