cc9c92171b6cc95c9c44721e3f74490c747c8bb8
Devana <devanaemmanuel@gmail.com>
2024-06-10 16:12:33 +0530

    pyramid

--- pyramid.py
+++ pyramid.py
@@ -2,11 +2,9 @@
 i = 0
 n = int(input("Enter the limit: "))
 for i in range(1, n + 1):
-    # Print leading spaces
     for j in range(n - i):
         print(" ", end="")
-    
-    # Print asterisks for the current row
+
     for k in range(1, 2*i):
         print("*", end="")
     print()
