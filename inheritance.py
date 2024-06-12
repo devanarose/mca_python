bf5c46e2101e6a37f1612e53cb56b0a331d044c8
Devana <devanaemmanuel@gmail.com>
2024-06-12 14:45:34 +0530

    june 12

--- inheritance.py
+++ inheritance.py
@@ -1,20 +1,37 @@
-class animal :
-    def __init__(self,name,breed):
-        self.name=name
-        self.breed=breed
+# class animal :
+#     def __init__(self,name,breed):
+#         self.name=name
+#         self.breed=breed
     
-    def printname(self):
-        print(self.name,self.breed)
+#     def printname(self):
+#         print(self.name,self.breed)
 
-class cat(animal):
-    def __init__(self,name,breed):
-        super().__init__(name,breed)
-        self.sound="MEOW"
+# class cat(animal):
+#     def __init__(self,name,breed):
+#         super().__init__(name,breed)
+#         self.sound="MEOW"
     
-    def printname(self):
-        print(self.name,self.breed,self.sound)
+#     def printname(self):
+#         print(self.name,self.breed,self.sound)
         
-x = animal("Dog","retriever")
-x.printname()
-c = cat("punchi","ragdoll")
-c.printname()
+# x = animal("Dog","retriever")
+# x.printname()
+# c = cat("punchi","ragdoll")
+# c.printname()
+
+
+
+class cat :
+    def printt(self):
+        print("meow")
+class dog :
+    def printt(self):
+        print("bow bow")
+class cow :
+    def printt(self):
+        print("moooo")
+
+cat().printt()
+dog().printt()
+cow().printt()
+    
