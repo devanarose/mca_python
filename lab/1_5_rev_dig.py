d8ab480357cb99f9aad72a7f6bd3f5a143193f31
Devana <devanaemmanuel@gmail.com>
2024-06-19 15:13:15 +0530

    rev digit

--- lab/1_5_rev_dig.py
+++ lab/1_5_rev_dig.py
@@ -6,12 +6,14 @@ def is_palindrome(num):
 def reverse_and_add(num):
 
   while True:
-    reversed_num = int(str(num)[::-1])
-    sum_of_digits = num + reversed_num
-    if is_palindrome(sum_of_digits):
-      return sum_of_digits
-    else:
-      num = sum_of_digits
+      if is_palindrome(num):
+          return num
+      reversed_num = int(str(num)[::-1])
+      sum_of_digits = num + reversed_num
+      if is_palindrome(sum_of_digits):
+        return sum_of_digits
+      else:
+            num = sum_of_digits
 
 # Example usage
 number = int(input("Enter number : "))
